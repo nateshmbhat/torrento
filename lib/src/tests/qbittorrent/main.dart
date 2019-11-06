697cac41b9516a8c7317a2795baff3637a5cf0ee
nateshmbhat <nateshmbhat1@gmail.com>
2019-11-06 06:12:45 +0530

    updated the testing code

--- lib/src/tests/qbittorrent/main.dart
+++ lib/src/tests/qbittorrent/main.dart
@@ -1,20 +1,21 @@
+import 'dart:io';
+
 import 'package:torrent_api/src/qbittorrent_api/qbittorrent_api.dart';
 
 main(List<String> args) async {
   QBitTorrentAPI obj = QBitTorrentAPI('192.168.0.100', 8080);
-  print(await obj.login('natesh', 'password') ? 'Login Success' : '');
-  print(await obj.getVersion());
-  print(await obj.getDefaultSavePath());
-  print(await obj.getWebApiVersion());
-  // print(await obj.getPreferences());
-  print(await obj.setPreferences({'max_active_downloads': 10}));
-  print(await obj.getTransferInfo());
-  print(await obj.getSpeedLimitsMode());
-  print(await obj.toggleSpeedLimitsMode());
-  print(await obj.getDownloadLimit());
-  print(await obj.getTorrentList(filter: TorrentFilter.downloading)) ; 
-  print(await obj.getTorrentContents('038caafe66d920547062d25245456d88c2715895')) ; 
+  await obj.login('natesh', 'password') ; 
 
-  print(await obj.logout() ? 'Logout Success' : '');
+
+  await obj.startAllTorrents() ;
+  print("Started all torrents. Sleeping for 5 seconds before stopping them all.") ; 
+  
+  sleep(Duration(seconds: 5));
+
+
+  await obj.stopAllTorrents() ;
+  print("Stopped all torrents") ; 
+
   print(await obj.getVersion());
+  await obj.logout() ; 
 }
