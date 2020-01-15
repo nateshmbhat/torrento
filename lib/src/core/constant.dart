3255a4def07959a9de5bb824a48fe6f92ad23052
nateshmbhat <nateshmbhat1@gmail.com>
2020-01-15 17:07:42 +0530

    implemented rest of the methods in the QbitTorrentController interface. Added Constant class to represent many of the string constants

---
+++ lib/src/core/constant.dart
@@ -0,0 +1,82 @@
+class Constant{
+  static const category= 'category' ; 
+  static const savePath = 'savePath' ; 
+  static const hash = 'hash' ; 
+  static const hashes = 'hashes' ; 
+  static const cookie = 'cookie' ; 
+  static const rename= 'rename' ; 
+  static const upLimit = 'upLimit' ; 
+  static const dlLimit = 'dlLimit' ; 
+  static const useAutoTMM= 'useAutoTMM' ; 
+  static const peers= 'peers' ;
+  static const zeroDigit= '0' ;
+  static const trueString ='true' ; 
+
+  static const password = 'password' ; 
+
+  static const username = 'username' ; 
+
+  static const json = 'json' ; 
+
+  static const normal = 'normal' ; 
+
+  static const info = 'info' ; 
+
+  static const warning = 'warning' ; 
+
+  static const critical = 'critical' ; 
+
+  static const last_known_id = 'last_known_id' ; 
+
+  static const rid = 'rid' ; 
+
+  static const limit = 'limit' ; 
+
+  static const filter = 'filter' ; 
+
+  static const sort = 'sort' ; 
+
+  static const reverse = 'reverse' ; 
+
+  static const offset = 'offset' ; 
+
+  static const deleteFiles = 'deleteFiles' ; 
+
+  static const urls = 'urls' ; 
+
+  static const torrents = 'torrents' ; 
+
+  static const skip_checking = 'skip_checking' ; 
+
+  static const paused = 'paused' ; 
+
+  static const root_folder = 'root_folder' ; 
+
+  static const sequentialDownload = 'sequentialDownload' ; 
+
+  static const categories = 'categories' ; 
+
+  static const tags = 'tags' ; 
+
+  static const enable = 'enable' ; 
+
+  static const all = 'all' ; 
+
+  static const origUrl = 'origUrl' ; 
+
+  static const newUrl = 'newUrl' ; 
+
+  static const value = 'value' ; 
+
+  static const firstLastPiecePrio = 'firstLastPiecePrio' ; 
+
+  static const ratioLimit = 'ratioLimit' ; 
+
+  static const seedingTimeLimit = 'seedingTimeLimit' ; 
+
+  static const name = 'name' ; 
+
+  static const id = 'id' ; 
+
+  static const priority = 'priority' ; 
+}
