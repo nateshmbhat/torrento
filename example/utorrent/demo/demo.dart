d0c6843d39e09d4ff058cf51f5a806d250b32141
nateshmbhat <nateshmbhat1@gmail.com>
2020-01-18 22:20:18 +0530

    version upate

--- example/utorrent/demo/demo.dart
+++ example/utorrent/demo/demo.dart
@@ -1,5 +1,4 @@
 import 'package:torrential_lib/src/torrent_client_controllers/u_torrent/u_torrent_controller.dart';
-import 'package:torrential_lib/torrential_lib.dart'  ; 
 
 UTorrentController obj = new UTorrentController(serverIp : '192.168.43.18' , serverPort :  5000) ; 
 
